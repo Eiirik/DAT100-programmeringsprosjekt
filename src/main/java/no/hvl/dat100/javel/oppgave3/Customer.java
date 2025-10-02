328cde8b6f67e40e3b31330fbd5e1998c4c46097
Eiirik <nurkeplays@hotmail.com>
2025-10-02 21:20:51 +0200

    Ferdig oppgave 3

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -2,15 +2,51 @@ package no.hvl.dat100.javel.oppgave3;
 
 public class Customer {
 
-    // TODO - object variables (attributes)
+    // Oppgave 3a) Klassen Customer
+    private String name;
+    private String email;
+    private int customer_id;
+    private PowerAgreementType agreement;
 
+    // Oppgave 3b) Konstruktør
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
-
-        // TODO
+        this.name = name;
+        this.email = email;
+        this.customer_id = customer_id;
+        this.agreement = agreement;
+    }
+        // Oppgave 3c) GET-metoder & SET-metoder
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public int getCustomer_id(){
+        return customer_id;
+    }
+    public PowerAgreementType getAgreement(){
+        return agreement;
     }
 
-    // TODO - getter/setter methods for all objectvariables
+    // SET-metoder
 
-    // TODO - toString method
+    public void setName(String nyName){
+        name = nyName;
+    }
+    public void setEmail(String nyEmail){
+        email = nyEmail;
+    }
+    public void setCustomer_id(int nyCustomer_id){
+        customer_id = nyCustomer_id;
+    }
+    public void setAgreement(PowerAgreementType nyAgreement){
+        agreement = nyAgreement;
+    }
+    // Oppgave 3d) toString metode
+    public  String toString(){
+        return "Customer number " + customer_id + "\nName " + name + "\nEmail " + email + "\nAgreement " + agreement;
+    }
+
 
 }
--- src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
@@ -11,11 +11,25 @@ public class CustomerMain {
         System.out.println("==============");
         System.out.println();
 
-        /*
-        TODO
+        Customer c1 = new Customer("Eirik", "test@test.com", 1001, PowerAgreementType.NORGESPRICE);
 
-         Write code that creates a Customer object and teste the methods implemented in the class
+        // Oppgave 3e) Test metodene i klassen.
 
-        */
+        // Printer ut alle verdiene via GET-metoder.
+        System.out.println(c1.getName());
+        System.out.println(c1.getEmail());
+        System.out.println(c1.getCustomer_id());
+        System.out.println(c1.getAgreement());
+
+        // Endrer alle verdiene via SET-metoder
+        c1.setName("John Olsen");
+        c1.setEmail("test123123@test123.com");
+        c1.setCustomer_id(5020);
+        c1.setAgreement(PowerAgreementType.SPOTPRICE);
+
+        System.out.println();
+
+        // Printer ut de nye verdiene med toString metoden.
+        System.out.println(c1.toString());
     }
 }
